8e71253b7a533efbce366caf9e809f4aaaca0eaa
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2024-09-06 19:23:28 -0300

    Commit inicial con proyecto recien iniciado.

---
+++ app/src/main/java/com/example/ucu2024_android_navigation_views_styles_leccion1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ucu2024_android_navigation_views_styles_leccion1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucu2024_android_navigation_views_styles_leccion1.ui.theme.Ucu2024androidnavigationviewsstylesleccion1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ucu2024androidnavigationviewsstylesleccion1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ucu2024androidnavigationviewsstylesleccion1Theme {
+        Greeting("Android")
+    }
+}
